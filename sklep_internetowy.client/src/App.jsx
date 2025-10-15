d5d89935ec0374e7252d5c192c71df25b38db0e4
Mikołaj Szczot <szczotmikolaj@gmail.com>
2025-10-15 22:34:34 +0200

    feat: add registration page template

--- sklep_internetowy.client/src/App.jsx
+++ sklep_internetowy.client/src/App.jsx
@@ -5,6 +5,8 @@ import { Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home/Home'
 import LoginPage from './pages/LoginPage/LoginPage'
+import Registration from './pages/Registration/Registration'
+
 
 function App() {
 
@@ -14,6 +16,7 @@ function App() {
         <Routes>
               <Route path="/" element={<Home />}></Route>
               <Route path="/login" element={<LoginPage />}></Route>
+              <Route path="/registration" element={<Registration />}></Route>
         </Routes>
     </>
   )
