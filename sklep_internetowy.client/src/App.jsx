ab2f49fb8b5b35cfd3be49d7efc80d83c493ff4e
Mateusz <example@mail.com>
2025-10-15 19:57:42 +0200

    Add footer with map and contact form

--- sklep_internetowy.client/src/App.jsx
+++ sklep_internetowy.client/src/App.jsx
@@ -1,15 +1,18 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home/Home'
+
 function App() {
-  //const [count, setCount] = useState(0)
+
 
   return (
     <>
-
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+        </Routes>
     </>
   )
 }
---
+++ sklep_internetowy.client/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/footer/Footer"
+
+function Home() {
+    return (
+        <>
+        
+            <h2 className="text-center m-5">Home page</h2>
+        <Footer/>
+        </>
+    )
+}
+
+export default Home;
