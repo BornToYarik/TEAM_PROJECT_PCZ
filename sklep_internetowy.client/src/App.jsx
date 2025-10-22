6db251bd40cb8d80970018acab93d129b8dff71a
Oleksii Pavlenko <iskragame43@gmail.com>
2025-10-22 23:11:21 +0200

    feat: Add UsersList component

--- sklep_internetowy.client/src/App.jsx
+++ sklep_internetowy.client/src/App.jsx
@@ -1,26 +1,42 @@
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar/Navbar'
-import Home from './pages/Home/Home'
-import LoginPage from './pages/LoginPage/LoginPage'
-import Registration from './pages/Registration/Registration'
-
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/Home/Home';
+import LoginPage from './pages/LoginPage/LoginPage';
+import Registration from './pages/Registration/Registration';
+import UsersList from "./pages/Users/UsersList";
 
 function App() {
+    const users = [
+        {
+            id: 1,
+            name: "Jan Kowalski",
+            email: "jan.kowalski@example.com",
+            role: "admin",
+            createdAt: "2024-09-15T10:30:00Z",
+        },
+        {
+            id: 2,
+            name: "Anna Nowak",
+            email: "anna.nowak@example.com",
+            role: "user",
+            createdAt: "2024-10-01T12:00:00Z",
+        },
+    ];
 
-
-  return (
-      <>
-        <Navbar />
-        <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/login" element={<LoginPage />}></Route>
-              <Route path="/registration" element={<Registration />}></Route>
-        </Routes>
-    </>
-  )
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/registration" element={<Registration />} />
+                <Route path="/users" element={<UsersList users={users} />} />
+            </Routes>
+        </>
+    );
 }
 
-export default App
+export default App;
