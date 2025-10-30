e9694bc109205547b2c1d318124b6c21b640f496
Oleksii Pavlenko <iskragame43@gmail.com>
2025-10-30 16:20:12 +0100

    feat: ISK-64 add registration, authorization and authentication

---
+++ Sklep_internetowy.Server/Controllers/Auth/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sklep_internetowy.Server.DTOs;
+using Sklep_internetowy.Server.Services.Auth;
+
+namespace Sklep_internetowy.Server.Controllers.Auth
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AccountService _accountServcice;
+
+        private readonly IConfiguration _configuration;
+
+        public AuthController(AccountService AccountServcice, IConfiguration configuration)
+        {
+            _accountServcice = AccountServcice;
+            _configuration = configuration;
+        }
+
+        public IActionResult Register([FromBody] RegisterUserRequest request)
+        {
+            _accountServcice.Register(request.UserName, request.Email, request.Password);
+            return Ok(new { message = "Rejestracja powiodla się pomyślnie" });
+        }
+
+
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            return Ok(_accountServcice.Login(request.Email, request.Password));
+        }
+    }
+}
---
+++ Sklep_internetowy.Server/DTOs/RegisterUserRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace Sklep_internetowy.Server.DTOs
+{
+    public record RegisterUserRequest(string UserName, string Email, string Password);
+}
