6d1c9471be0193b2a5b4d7eaff3bac071a76b8e1
Mikołaj Szczot <szczotmikolaj@gmail.com>
2025-11-25 22:31:02 +0100

    Login and user profile upgrade

--- Sklep_internetowy.Server/Migrations/20251125131828_iii.cs
+++ Sklep_internetowy.Server/Migrations/20251125131828_iii.cs
@@ -9,7 +9,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Sklep_internetowy.Server.Migrations
 {
     /// <inheritdoc />
-    public partial class ppp : Migration
+    public partial class iii : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
